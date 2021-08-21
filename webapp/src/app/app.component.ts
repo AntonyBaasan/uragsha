b66607e8448d9d683bd51b37a70559024aedc465
Antony Baasandorj <antony.baasan@gmail.com>
2021-08-21 15:38:30 -0400

    add facebook login

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, setPersistence, signInWithRedirect, inMemoryPersistence, GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 @Component({
   selector: 'app-root',
@@ -11,11 +11,22 @@ export class AppComponent {
   title = 'webapp';
   constructor(public auth: AngularFireAuth) {
   }
-  login() {
+  loginGoogle() {
     var provider = new GoogleAuthProvider();
+    // force to select an account, otherwise will login automatically
     provider.setCustomParameters({
       prompt: "select_account"
     });
+    this.login(provider);
+  }
+  loginFacebook() {
+    var provider = new FacebookAuthProvider();
+    // force reauthenticate, otherwise will login automatically
+    provider.setCustomParameters({ auth_type: 'reauthenticate' })
+    provider.addScope('public_profile,email');
+    this.login(provider);
+  }
+  private login(provider: any) {
     // const auth = getAuth();
     this.auth.signInWithPopup(provider);
   }
