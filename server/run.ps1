782b95681b6dfdcb768080ac646636773b72b6b1
Antony Baasandorj <antony.baasan@gmail.com>
2021-09-03 17:02:42 -0400

    add server run script

---
+++ server/run.ps1
@@ -0,0 +1,15 @@
+$option = Read-Host "Which project you want to run?
+    1. All [default]
+    2. Uragsha.Signalling
+    3. Uragsha.WebApi
+"
+Write-Output $option
+switch ($option)
+{
+    2 {Start-Process -FilePath 'dotnet' -WorkingDirectory '.' -ArgumentList 'run --project ./Uragsha.Signalling'; Break}
+    3 {Start-Process -FilePath 'dotnet' -WorkingDirectory '.' -ArgumentList 'run --project ./Uragsha.WebApi'; Break}
+    default {
+     Start-Process -FilePath 'dotnet' -WorkingDirectory '.' -ArgumentList 'run --project ./Uragsha.Signalling';
+     Start-Process -FilePath 'dotnet' -WorkingDirectory '.' -ArgumentList 'run --project ./Uragsha.WebApi'; 
+    }
+}
