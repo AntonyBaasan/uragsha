8cafee4279704efa3bcd15c476af43ab27bc5a1d
Antony Baasandorj <antony.baasan@gmail.com>
2021-09-08 09:36:43 -0400

    simplify using

--- server/Entity/UserEntityService.cs
+++ server/Entity/UserEntityService.cs
@@ -16,54 +16,44 @@ namespace Entity
 
         public void AddUser(User user)
         {
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
-                dbContext.Users.Add(user);
-                dbContext.SaveChangesAsync();
-            }
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            dbContext.Users.Add(user);
+            dbContext.SaveChangesAsync();
         }
 
         public void DeleteUser(string userId)
         {
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
-                var user = new User() { Id = userId };
-                dbContext.Attach(user);
-                dbContext.Remove(user);
-                dbContext.SaveChangesAsync();
-            }
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            var user = new User() { Id = userId };
+            dbContext.Attach(user);
+            dbContext.Remove(user);
+            dbContext.SaveChangesAsync();
         }
 
         public async Task<User> GetUserByIdAsync(string userId)
         {
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
-                var user = await dbContext.Users.FindAsync(userId);
-                return user;
-            }
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            var user = await dbContext.Users.FindAsync(userId);
+            return user;
         }
 
         public void UpdateUser(User user)
         {
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
-                dbContext.Users.Update(user);
-                dbContext.SaveChangesAsync();
-            }
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            dbContext.Users.Update(user);
+            dbContext.SaveChangesAsync();
         }
 
         public bool Exist(string userId)
         {
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
-                var exist = dbContext.Users.Any(user => user.Id.Equals(userId, System.StringComparison.Ordinal));
-                return exist;
-            }
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+            var exist = dbContext.Users.Any(user => user.Id.Equals(userId, System.StringComparison.Ordinal));
+            return exist;
         }
     }
 }
