df27e862d932282735b4263c514f8de4110688f8
Antony Baasandorj <antony.baasan@gmail.com>
2021-10-15 14:28:10 -0400

    rename session time

--- webapp/src/app/call/call-page.component.ts
+++ webapp/src/app/call/call-page.component.ts
@@ -35,8 +35,8 @@ export class CallPageComponent implements OnInit, OnDestroy {
   remoteUserSetting: UserCallMetadata;
 
   userId: string = '';
-  // secondsLeft: number = 900; // 15min
-  secondsLeft: number = 10;
+  sessionTimerInSeconds: number = 900;// 15min
+  // sessionTimerInSeconds: number = 10;
 
   private subOnWorkoutStateUpdated: Subscription | undefined;
   private subOnSessionDetailUpdated: Subscription | undefined;
