f6dd6dc3aad04a9d8559008f3ef160c6ffcb9fb3
Antony Baasandorj <antony.baasan@gmail.com>
2021-10-16 23:22:47 -0400

    add full-publish script

---
+++ full-publish.sh
@@ -0,0 +1,28 @@
+VERSION=${1:-latest}
+
+echo "Publish docker images and deploy to Kubernetes:" $VERSION
+
+# publish .net server applications
+cd server
+docker build -f ./Uragsha.Signalling/Dockerfile --force-rm -t antonybaasan/uragsha.signalling:$VERSION .
+docker push antonybaasan/uragsha.signalling:$VERSION
+
+docker build -f ./Uragsha.WebApi/Dockerfile --force-rm -t antonybaasan/uragsha.webapi:$VERSION .
+docker push antonybaasan/uragsha.webapi:$VERSION
+
+docker build -f ./Uragsha.Scheduler.HostedService/Dockerfile --force-rm -t antonybaasan/uragsha.scheduler:$VERSION .
+docker push antonybaasan/uragsha.scheduler:$VERSION
+
+docker build -f ./Uragsha.InstantMatcher.HostedService/Dockerfile --force-rm -t antonybaasan/uragsha.instantmatcher:$VERSION .
+docker push antonybaasan/uragsha.instantmatcher:$VERSION
+
+# publish angular web application
+cd ../webapp
+npm run build --aot --outputHashing=bundles
+docker build -t antonybaasan/uragsha.webapp:$VERSION  .
+docker push antonybaasan/uragsha.webapp:$VERSION
+
+cd ..
+helm upgrade uragsha-first --set appVersion=$VERSION ./devops/helm/uragsha-chart
+
+read -p "Press enter to continue"
