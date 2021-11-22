3510dae0fa9a47d123082560de32edc9b9d48ffb
Antony Baasandorj <antony.baasan@gmail.com>
2021-11-22 16:33:19 -0500

    redirect to login

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -3,28 +3,29 @@ import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
 const redirectLoggedInToCalendar = () => redirectLoggedInTo(['calendar']);
-const redirectNotLoggedInToHome = () => redirectUnauthorizedTo(['']);
+const redirectNotLoggedIn = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomeModule),
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectLoggedInToCalendar }
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./pages/home/home.module').then((m) => m.HomeModule),
+  //   canActivate: [AngularFireAuthGuard],
+  //   data: { authGuardPipe: redirectLoggedInToCalendar }
+  // },
+  { path: '', redirectTo: 'calendar', pathMatch: 'full' },
   {
     path: 'calendar',
     loadChildren: () =>
       import('./pages/calendar/calendar.module').then((m) => m.CalendarModule),
     canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectNotLoggedInToHome }
+    data: { authGuardPipe: redirectNotLoggedIn }
   },
   {
     path: 'sessions',
     loadChildren: () => import('./pages/session/session.module').then((m) => m.SessionModule),
     canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectNotLoggedInToHome }
+    data: { authGuardPipe: redirectNotLoggedIn }
   },
   {
     path: 'call',
@@ -35,7 +36,7 @@ const routes: Routes = [
     path: 'result',
     loadChildren: () => import('./pages/result/result.module').then((m) => m.ResultModule),
     canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectNotLoggedInToHome }
+    data: { authGuardPipe: redirectNotLoggedIn }
   },
   {
     path: 'login',
